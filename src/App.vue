b76bf93f63a0d5a0bc27457daa47f7461f6c0f79
aaropoikela <aaro.poikela@edu.lapinamk.com>
2024-04-25 11:20:46 +0300

    SentimentAnalysis added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import SentimentAnalysis from './components/SentimentAnalysis.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>Mobile friendly sentiment analysis</h1>
+      <SentimentAnalysis />
     </div>
   </header>
-
   <main>
-    <TheWelcome />
   </main>
 </template>
 
@@ -30,7 +28,7 @@ header {
 @media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: center;
+    place-items: bottom;
     padding-right: calc(var(--section-gap) / 2);
   }
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,11 +9,13 @@ defineProps({
 
 <template>
   <div class="greetings">
+    
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      add sentence or word to analysis and get a sentiment of positive or negative.
+      
+      <!--a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a-->.
     </h3>
   </div>
 </template>
